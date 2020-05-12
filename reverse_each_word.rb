b0eef73458fe6094f099f9124f48683c1e894b5f
Nicole Wang <nicole.wang@yale.edu>
2020-05-12 22:54:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
 def reverse_each_word(string)
-  array1 = string.split(" ")
-  array2 = []
-  array1.each do |string|
-    array2 << string.reverse
-  end
-  return array2.join(" ") 
+  
   
   array3 = string.split(" ")
   array4 {|x| x.reverse!}
